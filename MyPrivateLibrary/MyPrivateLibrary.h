630eb6676a1bf0ff9f0d1c1ba35e63ce329b3b74
survil <survil.easternts@gmail.com>
2024-09-09 17:24:37 +0530

    Initial Commit

---
+++ MyPrivateLibrary/MyPrivateLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyPrivateLibrary.h
+//  MyPrivateLibrary
+//
+//  Created by iMac on 09/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPrivateLibrary.
+FOUNDATION_EXPORT double MyPrivateLibraryVersionNumber;
+
+//! Project version string for MyPrivateLibrary.
+FOUNDATION_EXPORT const unsigned char MyPrivateLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPrivateLibrary/PublicHeader.h>
+
+
